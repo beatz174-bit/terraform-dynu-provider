5db7d5d8a88c070a83e8cfe11d172be359379e9e
beatz174-bit <beatz174@gmail.com>
2026-04-23 14:00:11 +1000

    Fix gofmt import indentation in dns record resource

--- internal/provider/resource_dns_record.go
+++ internal/provider/resource_dns_record.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
-		"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
